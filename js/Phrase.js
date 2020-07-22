dcd652f34581d271e82f0ece79ced0bb01bfb675
Stephenchow <39304873+stephencow@users.noreply.github.com>
2020-07-22 16:54:15 -0700

    Update Fix keypress

--- js/Phrase.js
+++ js/Phrase.js
@@ -34,6 +34,9 @@
          const letter = button.textContent;
          const correntPhrase = this.pharse;
          if(correntPhrase.indexOf(letter) > -1){
+            if(button.classList.contains("chosen")){
+                return false;
+            }
             return true;
          }else{
             return false;
